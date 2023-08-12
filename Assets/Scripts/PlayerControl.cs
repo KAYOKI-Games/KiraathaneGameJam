7a42a854133c2a5556f008d2041d7764232270ee
dilara <dilara.dcelik@gmail.com>
2023-08-12 17:48:34 +0300

    add all character animations on the player

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,22 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
-    public float speed = 10f; //Controls velocity multiplier
-    private Rigidbody2D rb;
+    [SerializeField]
+    private GameObject player;
+    public float speed = 12f; //Controls velocity multiplier
+    private Rigidbody2D playerRB;
+    private Animator anim;
+    private bool faceRight = true;
+    public float jumpSpeed, jumpFrequency, nextJumpTime;
+    public bool isGrounded;
+
+    public Transform groundCheckPosition;
+    public float groundCheckRadius;
+    public LayerMask groundCheckLayer;
     private void Start()
     {
-        rb = player.GetComponent<Rigidbody2D>();
+        playerRB = player.GetComponent<Rigidbody2D>();
+        anim = player.GetComponent<Animator>();
     }
 
     private void Update()
     {
-        float xMove = Input.GetAxisRaw("Horizontal"); // d key changes value to 1, a key changes value to -1
-        float yMove = Input.GetAxisRaw("Vertical"); // w key changes value to 1, s key changes value to -1
+        HorizontalMove();
+        OnGroundCheck();
+        
+        if(playerRB.velocity.x < 0 && faceRight) FacialMovement();
+        else if(playerRB.velocity.x>0 && !faceRight) FacialMovement();
 
-        rb.velocity = new Vector3(xMove,yMove) * speed;
+        if (Input.GetAxis("Vertical") > 0 && isGrounded && nextJumpTime < Time.timeSinceLevelLoad)
+        {
+            nextJumpTime = Time.timeSinceLevelLoad + jumpFrequency;
+            Jump();
+        }
     }
+
+    void HorizontalMove()
+    {
+        playerRB.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, playerRB.velocity.y);
+        anim.SetFloat("playerSpeed",Mathf.Abs(playerRB.velocity.x));
+    }
+
+    void Jump()
+    {
+        playerRB.AddForce(new Vector2(0, jumpSpeed));
+    }
+
+    void FacialMovement()
+    {
+        faceRight = !faceRight;
+        var transformLocalScale = transform.localScale;
+        transformLocalScale.x *= -1;
+        transform.localScale = transformLocalScale;
+        
+    }
+
+    void OnGroundCheck()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheckPosition.position,groundCheckRadius,groundCheckLayer); 
+        anim.SetBool("IsGroundedAnim",isGrounded);
+    }
+
 }
