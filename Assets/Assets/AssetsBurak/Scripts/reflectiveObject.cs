12f862021bfb35bf4cc022a5cee83a5625501422
burakbasher <burak.baser_@hotmail.com>
2023-08-13 11:13:21 +0300

    set Prefabs

---
+++ Assets/Assets/AssetsBurak/Scripts/reflectiveObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reflectiveObject : MonoBehaviour
+{
+    void Start()
+    {
+        InvokeRepeating("ToggleObject", 0.1f, 0.7f);
+    }
+
+
+    private void ToggleObject()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+
+
+}
